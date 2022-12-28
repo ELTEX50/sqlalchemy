374c3a8d9170b0fcca6f0217db7da183f032fc5d
ELTEX <89698269+ELTEX50@users.noreply.github.com>
2022-12-29 00:55:10 +0330

    Add files via upload

---
+++ sql2 update.py
@@ -0,0 +1,25 @@
+from sqlalchemy import*
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engin=create_engine("mysql+mysqlconnector://root:12345678fgh@localhost:3306/pr",echo=False)
+
+Session=sessionmaker(bind=engin)
+session=Session()
+
+base=declarative_base()
+
+class st(base):
+    __tablename__="pr1"
+
+    id=Column(Integer,primary_key=True, autoincrement=True)
+    name=Column(String(50))
+    age=Column(Integer)
+    grade=Column(String(50))
+
+st1=session.query(st)
+
+st1=session.query(st).filter(st.name=="eminem").first()
+st1.name="per"
+session.commit()
+# print(st1.name)
